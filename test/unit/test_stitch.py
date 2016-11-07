05db239429384c81a55952c48822148bc2e3cc83
Jeffrey Chan <jchan@clytius.EECS.Berkeley.EDU>
2016-11-07 14:59:58 -0800

    With the tests added

--- test/unit/test_stitch.py
+++ test/unit/test_stitch.py
@@ -1,7 +1,11 @@
+from __future__ import print_function, division
 import numpy as np
 import smcpp._smcpp, smcpp.model, smcpp.spline, smcpp.util, smcpp.estimation_tools
 import scipy.optimize
+import sys
+import os.path
 
+# Jonathan's test for random data
 def test_stitch():
     hs = np.concatenate([[0.0], np.logspace(-3, 1, 30), [np.inf]])
     s = np.diff(hs)[:10]
@@ -45,20 +49,25 @@ class StitchOptimizer():
         for c in contigs:
             obs.append(np.array(c.data,dtype=np.int32))
 
-        print obs[0]
-
         # Initialize IM
         n = 50
         self._im = smcpp._smcpp.PyOnePopInferenceManager(n - 2, obs , hs, 0, stitchpoints, None)
         self._im.model = model
         self._im.theta = 0.0025
         self._im.rho = 0.0025
-        self._im.rho_vals[:] = [0.001, 0.01, 0.001]
+        self._im.rho_vals[:] = [0.0025]*(len(stitchpoints) - 1)
+        #self._im.rho_vals[:] = [0.0025]
 
 
     def optimize(self, x0):
-        bnds = ((0.00001, 0.1), (0.0001, 0.1), (0.00001, 0.01))
+        #bnds = [(0.0,0.5)]
+        bnds = [(0.001,0.2)]*len(x0)
         return scipy.optimize.minimize(self.helper, x0, bounds=bnds)
+
+    def ll_surface(self, rhos, mode_left, mode_right):
+        compute = lambda r: self.helper([mode_left, r, mode_right])
+        ll = [-1.0*compute(r) for r in rhos]
+        return ll
 
     def helper(self, rhovals):
         self._im.rho_vals[:] = rhovals
@@ -69,15 +78,115 @@ class StitchOptimizer():
 
         return -1.0*self._im.loglik()
 
+def optimizer(stitchpoints, filename, num_composite = 25):
+    files = []
+    pi = np.random.permutation(25)
+    for i in pi:
+        file = "stitched_data/" + filename + ".smc." + str(i) + ".txt"
+        print(file)
+        files.append(file)
+    so = StitchOptimizer(stitchpoints, files)
+    x = so.optimize([0.008] * (len(stitchpoints)- 1))
+    return x
+
+# Tests flat human recomb with no stitchpoints
+def test_no_hotspot(filename, num_composite):
+    stitchpoints = [0,100000]
+    optimal = optimizer(stitchpoints, filename , num_composite)
+    return optimal
+
+def test_hotspot(filename, hotspot_kb=2, num_composite=25):
+    left = 50000 - hotspot_kb*1000/2
+    right = 50000 + hotspot_kb*1000/2
+    stitchpoints = [0,left,right,100000]
+    print("Stitchpoints: ",stitchpoints)
+    optimal = optimizer(stitchpoints, filename, num_composite)
+    return optimal
+
+# Tests flat human recomb with stitchpoints
+# Width of middle stitchpoints
+# num_composite - The number of samples for the composite likelihood
+def test_flat(filename, out, num_composite=25):
+    return print(filename, ": ", test_no_hotspot(filename,num_composite).x, file=out)
+
+def test_tenx(length, out,  num_composite=25):
+    print("Tenx_" + str(length) + "k:" ,test_hotspot("tenx_" + str(length) + "k", hotspot_kb=length).x, file=out)
+
+def test_fiftyx(length, out, num_composite=25):
+    print("Fiftyx_" + str(length) + "k:", test_hotspot("fiftyx_" + str(length) + "k", hotspot_kb=length).x, file=out)
+
+def test_hundredx(length, out, num_composite=25):
+    print("Hundredx_" + str(length) + "k:", test_hotspot("hundredx_" + str(length) + "k", hotspot_kb = length).x, file=out)
+
+
+def save_likelihood_surface(surface_file, modes_file):
+    rhos = np.linspace(0.0044, 0.1, 100)
+    stitchpoints = [0,47500,52500,100000]
+    modes = np.loadtxt(modes_file)
+    replicates = len(modes[:,0])
+
+    arr = np.zeros((25*replicates + 1, len(rhos)))
+    #arr[0,:] = rhos ## STILL NEED TO SAVE STUFF! How do we want to store? Single file or multiple?
+    for i in range(replicates):
+        for j in range(25):
+            files = ["stitched_data/tenx_5k_" + str(i) + ".smc." + str(j) + ".txt"]
+            so = StitchOptimizer(stitchpoints, files)
+            lls = so.ll_surface(rhos,modes[i,0],modes[i,2])
+            arr[25*i+j,:] = lls
+
+    arr[-1,:] = rhos
+    np.savetxt(surface_file, arr)
+
+def compute_modes(replicates):
+    stitchpoints = [0,47500,52500,100000]
+    print("Stitchpoints: ",stitchpoints)
+    arr = np.zeros((replicates,3))
+    for i in range(replicates):
+        files = []
+        for j in range(25):
+            files.append("stitched_data/tenx_5k_" + str(i) + ".smc." + str(j) + ".txt")
+        so = StitchOptimizer(stitchpoints, files)
+        opt = so.optimize([0.008] * (len(stitchpoints)- 1))
+        arr[i,:] = opt.x
+
+    np.savetxt("tenx_5k_modes", arr)
+
+
+
+
 
 
 
 
 if __name__ == "__main__":
-    stitchpoints = [0,49000,51000,100000]
-    files = []
-    for i in range(5):
-        file = "stitched_data/hundredx.smc." + str(i) + ".txt"
-        files.append(file)
-    so = StitchOptimizer(stitchpoints, files)
-    x = so.optimize([0.0025,0.025,0.0025])
+    save_likelihood_surface("tenx_5k_surfaces.txt", "tenx_5k_modes")
+    #compute_modes(20)
+    # fname = "composite_ll_variance.txt"
+    # if os.path.isfile(fname):
+    #     raise RuntimeError("Don't overwrite the file!!")
+    # else:
+    #     out = open(fname, "wt")
+    # rhos = np.linspace(0.0044, 0.1, 100)
+    # stitchpoints = [0,47500,52500,100000]
+    # #out = sys.stdout
+    # #test_hotspot("flat_new",hotspot_kb=5)
+    # # Tenx_5k: [ 0.00546596  0.04808182  0.00548466]
+
+
+    # #print("Rhos: ",list(rhos), file=out)
+    # replicates = 20
+    # arr = np.zeros((replicates + 1, len(rhos)))
+    # arr[0,:] = rhos
+    # for i in range(replicates):
+    #     print("Replicate: ", i, file=sys.stdout)
+    #     # subsample
+    #     pi = np.random.permutation(25)
+    #     files = []
+    #     for j in pi[:5]:
+    #         files.append("stitched_data/tenx_5k.smc." + str(j) + ".txt")
+    #     so = StitchOptimizer(stitchpoints, files)
+    #     lls = so.ll_surface(rhos)
+    #     arr[i+1,:] = lls
+
+    # np.savetxt(fname, arr)
+
--- util/simulate.sh
+++ util/simulate.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-source /export/home/users/jchan/opt/terhorst/bin/activate
+source /export/home/users/jchan/opt/terhorst_new/bin/activate
 export SCRM_PATH="/export/home/users/jchan/scrm"
-python /export/home/users/jchan/terhorst_psmcpp/psmcpp/util/scrm2vcf_variable.py 10 50000 5 -theta 68.75 -rho 60.5 -o "tenx" -sr 24000 605 -sr 26000 60.5
+
+#python /export/home/users/jchan/terhorst_psmcpp/psmcpp/util/scrm2vcf.py 25 2200.0 100000 -o "flat_5x" --demography "human" -t 500.0
+#python /export/home/users/jchan/terhorst_psmcpp/psmcpp/util/scrm2vcf.py 25 8800.0 100000 -o "flat_20x" --demography "human" -t 500.0
+#python /export/home/users/jchan/terhorst_psmcpp/psmcpp/util/scrm2vcf.py 25 22000.0 100000 -o "flat_50x" --demography "human" -t 500.0
+#python /export/home/users/jchan/terhorst_psmcpp/psmcpp/util/scrm2vcf.py 25 33000.0 100000 -o "flat_75x" --demography "human" -t 500.0
+#python /export/home/users/jchan/terhorst_psmcpp/psmcpp/util/scrm2vcf.py 25 44000.0 100000 -o "flat_100x" --demography "human" -t 500.0
+#python /export/home/users/jchan/terhorst_psmcpp/psmcpp/util/scrm2vcf.py 25 88000.0 100000 -o "flat_200x" --demography "human" -t 500.0
+#python /export/home/users/jchan/terhorst_psmcpp/psmcpp/util/scrm2vcf.py 25 220000.0 100000 -o "flat_500x" --demography "human" -t 500.0
+for i in {0..19}
+do
+   python /export/home/users/jchan/terhorst_psmcpp/psmcpp/util/scrm2vcf.py 25 440.0 100000 -o "tenx_5k_$i" --demography "human" -t 500.0 -sr 47500 4400.0 -sr 52500 440.0
+done
+#python /export/home/users/jchan/terhorst_psmcpp/psmcpp/util/scrm2vcf.py 25 440.0 100000 -o "fiftyx_15k" --demography "human" -t 500.0 -sr 42500 22000.0 -sr 57500 440.0
+#python /export/home/users/jchan/terhorst_psmcpp/psmcpp/util/scrm2vcf.py 25 440.0 100000 -o "hundredx_15k" --demography "human" -t 500.0 -sr 42500 44000.0 -sr 57500 440.0
 
