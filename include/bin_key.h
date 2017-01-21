3b5846741eadc23883f71d9d40edfb809f5d390f
Jonathan Terhorst <terhorst@gmail.com>
2017-01-21 11:12:10 -0800

    re-enable folding

--- include/bin_key.h
+++ include/bin_key.h
@@ -22,9 +22,9 @@ template <typename Derived1>
 std::set<block_key> bin_key<P>::run(
         const block_key &key, 
         const Eigen::MatrixBase<Derived1> &na,
-        const bool enabled)
+        const bool fold)
 {
-    return bin_key<P>::run(key.vals, na, enabled);
+    return bin_key<P>::run(key.vals, na, fold);
 }
 
 template <>
@@ -32,7 +32,7 @@ template <typename Derived1, typename Derived2>
 std::set<block_key> bin_key<1>::run(
         const Eigen::MatrixBase<Derived1> &key, 
         const Eigen::MatrixBase<Derived2> &na,
-        const bool enabled)
+        const bool fold)
 {
     Vector<int> tmp = key;
     std::set<block_key> init, ret;
@@ -45,19 +45,26 @@ std::set<block_key> bin_key<1>::run(
         }
     else
         init.emplace(tmp);
-    if (not enabled)
+    if (not fold)
         return init;
+    // Formerly this did binning, but now we use it for folding the
+    // ancestral allele. (A particular form of binning).
     for (const block_key &k : init)
     {
-        const int nseg = k(0) + k(1);
-        const int nb = k(2);
-        for (int aa = std::max(0, nseg - nb); aa <= std::min(na(0), nseg); ++aa)
-        {
-            const int bb = nseg - aa;
-            tmp(0) = aa;
-            tmp(1) = bb;
-            ret.emplace(tmp);
-        }
+        ret.emplace(k);
+        tmp(0) = na(0) - k(0);
+        tmp(1) = k(2) - k(1);
+        ret.emplace(tmp);
+        // Old binning rule (not used anymore)
+        // const int nseg = k(0) + k(1);
+        // const int nb = k(2);
+        // for (int aa = std::max(0, nseg - nb); aa <= std::min(na(0), nseg); ++aa)
+        // {
+        //     const int bb = nseg - aa;
+        //     tmp(0) = aa;
+        //     tmp(1) = bb;
+        //     ret.emplace(tmp);
+        // }
     }
     return ret;
 }
@@ -67,10 +74,10 @@ template <typename Derived1, typename Derived2>
 std::set<block_key> bin_key<P>::run(
         const Eigen::MatrixBase<Derived1> &key, 
         const Eigen::MatrixBase<Derived2> &na,
-        const bool enabled)
+        const bool fold)
 {
-    std::set<block_key> bk1 = bin_key<1>::run(key.head(3), na.head(1), enabled);
-    std::set<block_key> bk2 = bin_key<P - 1>::run(key.tail(3 * (P - 1)), na.tail(P - 1), enabled);
+    std::set<block_key> bk1 = bin_key<1>::run(key.head(3), na.head(1), fold);
+    std::set<block_key> bk2 = bin_key<P - 1>::run(key.tail(3 * (P - 1)), na.tail(P - 1), fold);
     std::set<block_key> ret;
     Vector<int> v(3 * P);
     for (const block_key& b1 : bk1)
